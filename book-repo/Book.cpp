b25fa70a4d4c4587d3be80665d03d12be576aa38
carson harder-hyde <carsonmhyde@gmail.com>
2020-11-11 17:22:26 -0500

    adds Repo.h, repo header

--- book-repo/Book.cpp
+++ book-repo/Book.cpp
@@ -18,5 +18,44 @@ Book::Book()//parameterless constructor - sets to default values
 
 Book::Book(string bookID, string title, string author, double prices[], BookType bookType)//full constructor
 {
-    
+    //(pointer_name)->(variable_name)
+    this->bookID = bookID;//always use "this"
+    this->title = title;
+    this->author = author;
+    for (int i = 0; i < priceArraySize; i++) this->prices[i] = this->prices[i];
+    this->bookType = bookType;
+}
+
+Book::~Book(){}//destructor does nothing because nothing is created dynamically but write it anyway; always write the desctructor
+//getters or accessors
+string Book::getID() {return this->bookID;}
+string Book::getTitle() {return this->title;}
+string Book::getAuthor() {return this->author;}
+double* Book::getPrices() {return this->prices;}//array name is a pointer; you don't want to return prices individually but the reference to them
+BookType Book::getBookType() {return this->bookType;}
+
+//setters or mutators; needs incoming parameter
+void Book::setID(string ID) { this->bookID = ID;}
+void Book::setTitle(string title) { this-> title = title;}
+void Book::setAuthor(string author) { this->author = author;}
+void Book::setPrices(double prices[])//set each price individually
+{
+    for (int i = 0; i < priceArraySize; i++) this->prices[i] = this->prices[i];
+}
+void Book::setBookType (BookType bt) { this->bookType = bt;}
+
+void Book::printHeader()//print header
+{
+    cout << "Output format: ID|Title|Author|Prices|Type\n";//format of printout
+}
+
+void Book::print()
+{
+    cout << this->getID() << '\t';//tab delimited
+    cout << this->getTitle() << '\t';
+    cout << this->getAuthor() << '\t';
+    cout << this->getPrices()[0] << ',';//grab prices individually instead of pointer
+    cout << this->getPrices()[1] << ',';//comma delimeter
+    cout << this->getPrices()[2] << ',';
+    cout << bookTypeStrings[this->getBookType()] << '\n'//bookType to string; is an int but uses BookType.h for string
 }
--- book-repo/Repo.hpp
+++ book-repo/Repo.hpp
@@ -4,10 +4,39 @@
 //
 //  Created by carson harder-hyde on 11/11/20.
 //
-
+#pragma once
+#include "Book.hpp"
 #ifndef Repo_hpp
 #define Repo_hpp
-
 #include <stdio.h>
+
+class Repo
+{
+private:
+    int lastIndex = -1;
+    const static int numBooks = 5;
+    Book* bookRepoArray[numBooks]; //not decalred dynamically
+    
+public:
+    //parse method parses set of comma delimted strings and extracts data
+    void parse(string row);
+    //pulls book data and creates book object in repo
+    void add(string bID,
+             string bTitle,
+             string bAuthor,
+             double bprice1,
+             double bprice2,
+             double bprice3,
+             BookType bt);
+    //once book object created, can call functions on it
+    void printAll();//calls print() for each book
+    void printByBookType(BookType bt);//book type passed in, print by book type
+    void printInvalidIDs();//each id needs an underscore and either an uppercase "X" or lowercase "x"
+    void printAveragePrices();//prints average price for each book
+    void removeBookByID(string bookID);//find book and remove
+    ~Repo();//destructor
+    //if you don't write a constructor, c++ provides and sets all instances to 0
+};
+
 
 #endif /* Repo_hpp */
